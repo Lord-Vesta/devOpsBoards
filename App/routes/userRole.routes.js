8f645447ef93377c3d9543049fe884ff9b13aede
Lord-Vesta <yashvardhan.jadhav@ellicium.com>
2024-04-21 13:38:43 +0530

    PATCH:added jwt authentication for userRole api's

--- App/routes/userRole.routes.js
+++ App/routes/userRole.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import userRoleControllers from '../controllers/userRoles.controllers.js';
+import { authenticateJwtToken } from '../../Middlewares/jwtAuthMiddleware.js';
 
 export const router = express.Router();
 
-router.get('/:UserId/roles',userRoleControllers.getRolesOfUser)
-router.post('/:userId/roles/:roleId',userRoleControllers.addRolesToUsers)
-router.delete('/:userId/roles/:roleId',userRoleControllers.deleteRoleofUser)
+router.get('/:UserId/roles',authenticateJwtToken,userRoleControllers.getRolesOfUser)
+router.post('/:userId/roles/:roleId',authenticateJwtToken,userRoleControllers.addRolesToUsers)
+router.delete('/:userId/roles/:roleId',authenticateJwtToken,userRoleControllers.deleteRoleofUser)
 
